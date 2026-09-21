752464ef59bd9355cf72889e1580c1aed168ea52
Stndr1506 <stndr1506@gmail.com>
2026-09-21 11:15:59 +0530

    backend and frontend update

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,26 @@ const serviceRoutes = require("./routes/serviceRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://rgbhaismmpanel-fhxp-roan.vercel.app",
+];
 
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
